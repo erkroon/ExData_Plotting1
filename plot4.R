469364f038cd094f593b68631f166f3767e772ed
Eric Kroon <eric_kroon@yahoo.com>
2014-10-12 12:30:53 +0300

    Create plot4.R

---
+++ plot4.R
@@ -0,0 +1,47 @@
+library(utils)
+library(datasets)
+fileURL <- "https://d396qusza40orc.cloudfront.net/exdata%2Fdata%2Fhousehold_power_consumption.zip"
+download.file(fileURL, "./power.zip")  
+unzip("./power.zip")
+datafile <- read.table("./household_power_consumption.txt", sep = ";", header= TRUE, na= "?", colClasses=c("character", "character", rep("numeric",7)))
+
+#Convert the date and time 
+datafile$Time <- strptime(paste(datafile$Date, datafile$Time), "%d/%m/%Y %H:%M:%S")
+datafile$Date <- as.Date(datafile$Date, "%d/%m/%Y")
+
+##get the two days
+dates <- as.Date(c("2007-02-01", "2007-02-02"), "%Y-%m-%d")
+newfile <- subset(datafile, datafile$Date %in% dates)
+
+# create the plots
+png(filename = "plot4.png",
+    width = 480, height = 480,
+    units = "px", bg = "transparent")
+par(mfrow = c(2, 2)) # 4 graphs
+# graph 1
+plot(newfile$Time, newfile$Global_active_power,
+     type = "l",
+     xlab = "", ylab = "Global Active Power")
+# graph 2
+plot(newfile$Time, newfile$Voltage,
+     type = "l",
+     xlab = "datetime", ylab = "Voltage")
+# graph 3
+plot(newfile$Time, newfile$Sub_metering_1,
+     type = "l",
+     col = "black",
+     xlab = "", ylab = "Energy sub metering")
+lines(newfile$Time, newfile$Sub_metering_2, col = "red")
+lines(newfile$Time, newfile$Sub_metering_3, col = "blue")
+# Remove the border of legend here.
+legend("topright",
+       bty = "n",
+       col = c("black", "red", "blue"),
+       c("Sub_metering_1", "Sub_metering_2", "Sub_metering_3"),
+       lwd = 1)
+# graph 4
+plot(newfile$Time, newfile$Global_reactive_power,
+     type = "l",
+     col = "black",
+     xlab = "datetime", ylab = colnames(newfile)[4])
+dev.off()
